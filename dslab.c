9c80c346d23c98471bc9f7e8dac32b420ae6573c
sripath-vs <59191553+sripath-vs@users.noreply.github.com>
2022-02-16 19:23:51 +0530

    Add files via upload

---
+++ dslab.c
@@ -0,0 +1,64 @@
+
+#include <stdio.h>
+
+int main()
+{   int r,choice;
+    
+    printf("Enter Roll no: ");
+    scanf("%d",&r);
+    
+    printf("Program List\n\n\n")
+    
+  
+        printf("[1] Implementation FTP Client\n");
+        printf("[2] Implementation of Name Server using RMI\n");
+        printf("[3] Explain the working of NFS\n");
+        printf("[4] Implementation of Chat Server\n");
+        printf("[5] Implementation of Chat Client\n");
+        printf("[6] Implementation of Bulletin Board\n");
+        printf("[7] Implement DNS using java\n");
+        
+        
+    printf("Enter your choice: \n");
+    scanf("%d",&choice);
+    
+    switch(choice)
+    {
+        case 1:{
+            system("wget ''");
+            break;
+        }
+        case 2:{
+            system("wget ''");
+            break;
+        }
+        case 3:{
+            system("wget ''");
+            break;
+        }
+        case 4:{
+            system("wget ''");
+            break;
+        }
+        case 5:{
+            system("wget ''");
+            break;
+        }
+        case 6:{
+            system("wget ''");
+            break;
+        }
+        case 7:{
+            system("wget ''");
+            break;
+        }
+        
+        
+    }
+        
+        
+    
+    
+
+    return 0;
+}
